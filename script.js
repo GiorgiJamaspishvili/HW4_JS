90a9faba36038d8f6ce3ecbd3d62a92035b1373e
GiorgiJamaspishvili <126399819+GiorgiJamaspishvili@users.noreply.github.com>
2023-05-10 18:09:56 +0400

    up

---
+++ script.js
@@ -0,0 +1,74 @@
+'use strict'
+
+
+// 1.
+// let array = [5, 25, 89, 120, 36];
+
+// array.push('JS', 'python');
+// console.log(array);
+
+// array.unshift('html','css');
+// console.log(array);
+
+// array.shift();
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+// let deleteElement = array.splice(0, 2, 'JS', 5);
+// console.log(array);
+
+
+
+// 2. შექმენით მასივი, შემდეგი ელემენტებით: ფორთოხალი, ბანანი, მსხალი;
+
+//   გამოიტანეთ ინფორმაცია ეკრანზე თუ რამდენი ელემენტი გაქვთ მასივში;
+//   მასივის ბოლოს დაამატეთ ორი ელემენტი: ვაშლი და ანანასი;
+//   მასივის დასაწყისში კი დაამატეთ ელემენტი: საზამთრო;
+//   გამოიტანეთ ეკრანზე, რამდენი ელემენტი გაქვთ მასივში;
+//   მასივის მე-3 ადგილას დაამატეთ ელემენტი: მანგო;
+//   წაშალეთ მასივიდან პირველი და ბოლო ელემენტი;
+//   გამოიტანეთ ეკრანზე, თუ რამდენი ელემენტი დარჩა მასივში;
+
+
+// let array2 = ['ფორთოხალი', 'ბანანი', 'მსხალი'];
+
+// array2.push('ვაშლი', 'ანანასი');
+// array2.unshift('საზამთრო');
+// array2.splice(3,0, 'მანგო');
+// array2.shift();
+// array2.pop();
+
+// console.log(array2);
+
+
+
+
+// 3.
+// let array3 = [1, 2, 3, 4, 5];
+// array3.splice(3,0, 'a', 'b', 'c');
+// console.log(array3);
+
+
+
+// 4
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach(function(i){
+//     let sum = 0;
+//     sum += i;
+//     console.log(sum);
+// })
+
+
+// 5.
+// let x = 100;
+// x.reverse();
+
+// let item = 100;
+// for(i=100; i<=100; i--){
+//     while (i = 0)
+//   console.log(i)
+// }
+// array.reverse();
+
